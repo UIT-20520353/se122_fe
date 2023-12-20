9b2d9e667df6d12c8b17fc16e5ca2e3af715cfbe
vuongtx21 <vuongtx@SparkMinds.net>
2023-12-20 17:48:40 +0700

    update ui for header

--- src/components/commons/Header.tsx
+++ src/components/commons/Header.tsx
@@ -1,18 +1,48 @@
 import React from "react";
 import { IoNotificationsOutline } from "react-icons/io5";
+import { AiOutlineMessage } from "react-icons/ai";
 import logo from "../../assets/images/logo.png";
+import default_avatar from "../../assets/images/avatar.png";
 import "./Header.style.css";
+import { Link } from "react-router-dom";
 
 interface HeaderProps {}
 
 const Header: React.FunctionComponent<HeaderProps> = () => {
   return (
     <div className="header">
-      <div className="header__logo">
+      <Link to={"/"} className="header__logo header__left">
         <img className="logo" src={logo} alt="logo" />
-      </div>
+        <div className="website-name">
+          <p>Ielts Tinder</p>
+          <p>Communication</p>
+        </div>
+      </Link>
 
-      <IoNotificationsOutline className="header__icon" />
+      <div className="header__right">
+        <div className="header__actions">
+          <IoNotificationsOutline className="header__icon" />
+          <AiOutlineMessage className="header__icon" />
+        </div>
+        <div className="header__profile">
+          <div className="user-detail">
+            <p>
+              Hello, <span>Xuan Vuong</span>
+            </p>
+            <p>User</p>
+          </div>
+          <img className="avatar" src={default_avatar} alt="avatar" />
+
+          <div className="dropdown-menu">
+            <div className="">
+              <p>Profile</p>
+            </div>
+            <div className="">
+              <p>Logout</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
