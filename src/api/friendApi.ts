0fcc3b5ce2ead9fa530f2eba1ebb5c732b76371d
vuongtx21 <vuongtx@SparkMinds.net>
2024-01-03 18:03:56 +0700

    update api for friend

---
+++ src/api/friendApi.ts
@@ -0,0 +1,25 @@
+import { ACCESS_TOKEN_LOCAL_STORAGE_KEY } from "../consts/app";
+import { FriendModel } from "../models/friend";
+import { BaseRequestQueryParam, HttpResponse } from "../models/http";
+import { getLocalStorage } from "../utils/localStorage";
+import axiosClient, { handleRequest } from "./axiosClient";
+
+const friendApi = {
+  getFriends: (
+    params: BaseRequestQueryParam
+  ): Promise<HttpResponse<FriendModel[]>> => {
+    const url = "/api/friends";
+    return handleRequest(
+      axiosClient.get(url, {
+        params,
+        headers: {
+          Authorization: `Bearer ${getLocalStorage(
+            ACCESS_TOKEN_LOCAL_STORAGE_KEY
+          )}`,
+        },
+      })
+    );
+  },
+};
+
+export default friendApi;
--- src/features/communication/pages/Friend.tsx
+++ src/features/communication/pages/Friend.tsx
@@ -1,8 +1,48 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useMemo, useState } from "react";
+import { FriendModel } from "../../../models/friend";
+import friendApi from "../../../api/friendApi";
+import { useAppDispatch } from "../../../app/hooks";
+import { setLoading } from "../../../redux/globalSlice";
+import { useHandleResponseError } from "../../../hooks/useHandleResponseError";
 
 interface FriendProps {}
 
+interface DataProps {
+  friends: FriendModel[];
+  totalUsers: number;
+}
+
 const Friend: React.FunctionComponent<FriendProps> = () => {
+  const dispatch = useAppDispatch();
+  const handleResponseError = useHandleResponseError();
+
+  const [data, setData] = useState<DataProps>({ friends: [], totalUsers: 0 });
+  const [page, setPage] = useState<number>(0);
+
+  const fetchData = async (page: number) => {
+    dispatch(setLoading("ADD"));
+    const { ok, body, error, pagination } = await friendApi.getFriends({
+      size: 3,
+      page,
+    });
+    dispatch(setLoading("REMOVE"));
+
+    if (ok && body && pagination) {
+      setData({ friends: body, totalUsers: pagination.total });
+    } else {
+      handleResponseError(error);
+    }
+  };
+
+  const totalPages = useMemo(() => {
+    return Math.ceil(data.totalUsers / 3);
+  }, [data.totalUsers]);
+
+  useEffect(() => {
+    fetchData(page);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page]);
+
   return <Fragment>dasdsadad</Fragment>;
 };
 
